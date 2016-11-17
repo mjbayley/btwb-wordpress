f75fe2435f5761989e3629fb5b39e4fa25c58449
Benjamin Yu <benjaminlyu@gmail.com>
2016-11-17 15:22:02 -0500

    SetCookie JWT to base path '/'

--- inc/BTWB_Class.php
+++ inc/BTWB_Class.php
@@ -256,7 +256,7 @@ if (!class_exists('BTWB_Class')) {
                     $btwbJwtToken = $_GET[BTWB_JWT_TOKEN];
 
                     /* Store/Update the locally stored scopes in cookies */
-                    setcookie(BTWB_LOCAL_PAGE_SCOPES, $btwbJwtToken);
+                    setcookie(BTWB_LOCAL_PAGE_SCOPES, $btwbJwtToken, 0, '/');
                     
                     /* Get BTWB Admin Settings */
                     $btwbSettings = json_decode(get_option(BTWB_SETTINGS_OPTION, 0));
